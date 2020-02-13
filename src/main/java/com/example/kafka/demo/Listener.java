8664966cf2d81d5d0db1b1eaabfa87b178af0804
Miguel Lima <miguelmoreiralima@outlook.com>
2020-02-13 17:48:52 -0300

    topic em kafka com partição

---
+++ src/main/java/com/example/kafka/demo/Listener.java
@@ -0,0 +1,24 @@
+package com.example.kafka.demo;
+
+import com.example.kafka.demo.model.Cliente;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Listener {
+    private static final Logger log = LoggerFactory.getLogger(Listener.class);
+
+    @KafkaListener(topics = "teste", concurrency = "3")
+    public void recebiMensagem(ConsumerRecord<String, Cliente> record) {
+        log.info("Mensagem recebida: {}, partição: {}, offset: {}, thread: {}", record.value(), record.partition(), record.offset(), Thread.currentThread().getId());
+    }
+
+    @KafkaListener(topics = "teste", groupId = "teste2")
+    public void recebiMensagem2(ConsumerRecord<String, Cliente> record) {
+        log.info("Mensagem recebida2: {}, partição: {}, offset: {}", record.value(), record.partition(), record.offset());
+    }
+}
